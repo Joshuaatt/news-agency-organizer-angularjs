5f0cac2c5923f79009a8bade83763c904dbb0ad7
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-02-27 14:40:48 -0800

    add show/hide reporter details

--- controllers/ReportersController.js
+++ controllers/ReportersController.js
@@ -1,7 +1,14 @@
 newsAgency.controller('ReportersCtrl', function ReportersCtrl($scope, $stateParams, BeatsFactory, UtilitiesFactory) {
   $scope.beat = UtilitiesFactory.findById(BeatsFactory.beats, $stateParams.beatId);
   $scope.addReporter = function() {
-    $scope.beat.reporters.push({ name: $scope.reporterName, years: $scope.yearsEmployed, birthday: $scope.reporterBirthday });
+    $scope.beat.reporters.push({
+      name: $scope.reporterName,
+      years: $scope.yearsEmployed,
+      birthday: $scope.reporterBirthday
+    });
+
     $scope.reporterName = null;
+    $scope.yearsEmployed = null;
+    $scope.reporterBirthday = null;
   }
 });
