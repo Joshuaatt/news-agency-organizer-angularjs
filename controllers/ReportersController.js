9c0bc3c2ea00fab19f61ce86018f09f33d3bcb29
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-02-27 13:06:37 -0800

    add controller to add reporters to a beat

---
+++ controllers/ReportersController.js
@@ -0,0 +1,7 @@
+newsAgency.controller('ReportersCtrl', function ReportersCtrl($scope, $stateParams, BeatsFactory, UtilitiesFactory) {
+  $scope.beat = UtilitiesFactory.findById(BeatsFactory.beats, $stateParams.beatId);
+  $scope.addReporter = function() {
+    $scope.beat.reporter.push({ name: $scope.reporterName });
+    $scope.reporterName = null;
+  }
+});
